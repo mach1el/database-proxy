f85fb8478009e0243a5251867bf5aeef98c3254d
t7hm1 <atemhacking@gmail.com>
2020-02-20 08:59:10 +0700

    Fix syntax acc table

--- init/odb2.sql
+++ init/odb2.sql
@@ -5,7 +5,6 @@ CREATE TABLE opensips_load_balancer (
     dst_uri VARCHAR(128) NOT NULL,
     resources VARCHAR(255) NOT NULL,
     probe_mode INTEGER DEFAULT 0 NOT NULL,
-    attrs VARCHAR(255) DEFAULT NULL,
     description VARCHAR(128) DEFAULT NULL
 );
 CREATE INDEX load_balancer_dsturi_idx ON opensips_load_balancer (dst_uri);
